786d656075f384687771b6220b94402ff400d68b
Gamakichii <75816283+Gamakichii@users.noreply.github.com>
2025-07-01 14:41:00 +0800

    Update App.js to Click Counter
    Commented out the Welcome function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/*
+function Welcome(props){
+  return <h2>Welcome, {props.name}!</h2>
+}
+
+function Show(props){
+  return <h2>{props.name}</h2>
+}
+*/
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+function handleClick(){
+  setCount(count + 1);
+}
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => handleClick(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    /*
+    <div style={{textAlign: "center"}}>
+      <Welcome name ="Vanderlei"/>
+      <Show name ="Bedania"/>
+      <Show name ="Mendoza"/>
+      
     </div>
+    */
+   <div>
+      <Counter />
+   </div>
   );
 }
 
